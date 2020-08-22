c36a0962286ae1771ba25df0e7f331ce641aba77
Prashant Goel <prashantmanunited@gmail.com>
2020-08-22 05:43:13 +0530

    feat: add react-axe

--- src/index.tsx
+++ src/index.tsx
@@ -7,12 +7,34 @@ import './index.css';
 
 import { unregister as unregisterServiceWorker } from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+const renderApp = () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root'),
+  );
+};
+
+if (process.env.NODE_ENV !== 'production') {
+  const handleError = (error: Error) => {
+    // eslint-disable-next-line no-console
+    console.error(error);
+  };
+
+  import('react-axe')
+    .then((axe) => {
+      axe
+        .default(React, ReactDOM, 1000)
+        .then(() => {
+          renderApp();
+        })
+        .catch(handleError);
+    })
+    .catch(handleError);
+} else {
+  renderApp();
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
