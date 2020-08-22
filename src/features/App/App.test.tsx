76c984474c9d75af3bb5ea1d110f40c3d86345f2
Prashant Goel <prashantmanunited@gmail.com>
2020-08-22 05:58:18 +0530

    feat: add jest-axe along with some tests

--- src/features/App/App.test.tsx
+++ src/features/App/App.test.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { axe, toHaveNoViolations } from 'jest-axe';
+
 import App from './App';
+
+expect.extend(toHaveNoViolations);
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
+
   expect(linkElement).toBeInTheDocument();
+});
+
+it('popup should not fail an aXe audit', async () => {
+  const { container } = render(<App />);
+
+  expect(await axe(container)).toHaveNoViolations();
 });
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,14 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+
+import { configureAxe } from 'jest-axe';
+
+// As we're testing on the JSDOM, color-contrast testing can't run.
+// The types of results fetched are limited for performance reasons
+configureAxe({
+  rules: {
+    'color-contrast': { enabled: false },
+  },
+  resultTypes: ['violations', 'incomplete'],
+});
